b434f3153d7080ef8f4c4b5fa3cf7a64eb11e03f
Vishrut Gupta <156781157+VishrutGupta@users.noreply.github.com>
2026-06-27 23:19:15 +0530

    Create constants.tsx

---
+++ src/constants.tsx
@@ -0,0 +1,19 @@
+export const PHONE = '9217747559';
+export const PHONE_TEL = 'tel:+919217747559';
+export const WHATSAPP_URL =
+  'https://wa.me/919217747559?text=Hi!%20I%27d%20like%20to%20book%20a%20Pilates%20session%20at%20Pilates%20Armour.';
+export const INSTAGRAM_URL = 'https://www.instagram.com/pilates_armour/';
+export const INSTAGRAM_HANDLE = '@pilates_armour';
+
+export const LOCATIONS = [
+  {
+    name: 'Sector 45, Gurgaon',
+    address: 'Sector 45, Gurugram, Haryana 122003',
+    mapsLink: 'https://www.google.com/maps/search/?api=1&query=Pilates+Studio+Sector+45+Gurgaon',
+  },
+  {
+    name: 'Sector 52, Gurgaon',
+    address: 'Sector 52, Gurugram, Haryana 122018',
+    mapsLink: 'https://www.google.com/maps/search/?api=1&query=Pilates+Studio+Sector+52+Gurgaon',
+  },
+];
