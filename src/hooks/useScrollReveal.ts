3e5c9af35cb502782fff05d6fbd59beead8bad8a
Vishrut Gupta <156781157+VishrutGupta@users.noreply.github.com>
2026-06-27 23:17:01 +0530

    Create useScrollReveal.ts

---
+++ src/hooks/useScrollReveal.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function useScrollReveal() {
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const elements = gsap.utils.toArray<HTMLElement>('.reveal');
+
+      elements.forEach((el) => {
+        gsap.to(el, {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: 'power3.out',
+          scrollTrigger: {
+            trigger: el,
+            start: 'top 85%',
+            toggleActions: 'play none none none',
+          },
+        });
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+}
